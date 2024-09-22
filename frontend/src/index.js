d5c93ea3cd1f5abfaf4c9d86c72134481f446823
Love Tewari <92184403+lovetewari@users.noreply.github.com>
2024-09-22 17:31:35 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import for React 18
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
